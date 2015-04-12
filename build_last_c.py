52105c5eced6ff4e0a10e98b91b6553a04c86d8c
Ezra Hoch <ezra.hoch@elastifile.com>
2015-04-12 18:05:33 +0300

    Now it really builds last

--- build_last_c.py
+++ build_last_c.py
@@ -5,14 +5,21 @@ last_filename = ""
 
 class BuildLastCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        if (not self.view.file_name().endswith('.t.c')):
-            if (self.view.file_name().endswith('.c')):
-                global last_filename
-                last_filename = self.view.file_name()
-
+        print(last_filename)
         window = sublime.active_window()
         window.open_file(last_filename)
 
         print(last_filename)
+        args = {"variant": "build_me"}
+        window.run_command('build', args)
+
+class BuildCurrentCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        global last_filename
+        last_filename = self.view.file_name()
+        print(last_filename)
+
+        window = sublime.active_window()
+
         args = {"variant": "build_me"}
         window.run_command('build', args)
