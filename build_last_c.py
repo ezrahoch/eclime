f77a317aad41058329a9c39bde8d8698525043e1
Ezra Hoch <ezra.hoch@elastifile.com>
2015-05-01 14:08:56 +0300

    build_last: WIP

--- build_last_c.py
+++ build_last_c.py
@@ -1,17 +1,25 @@
 import sublime, sublime_plugin
 import ntpath
+import time
 
 last_filename = ""
 
 class BuildLastCommand(sublime_plugin.TextCommand):
+    def build_now(self):
+        window = sublime.active_window()
+        args = {"variant": "build_me"}
+        window.run_command('build', args)
+
     def run(self, edit):
         print(last_filename)
         window = sublime.active_window()
-        window.open_file(last_filename)
+        new_view = window.open_file(last_filename)
 
-        print(last_filename)
-        args = {"variant": "build_me"}
-        window.run_command('build', args)
+        sublime.set_timeout(self.build_now, 100)
+
+        # if (new_view != self.view):
+        #     new_view.run_command("jump_back")
+
 
 class BuildCurrentCommand(sublime_plugin.TextCommand):
     def run(self, edit):
